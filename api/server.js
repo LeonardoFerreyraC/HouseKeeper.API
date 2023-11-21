8b718d03254cf2eeaf8828865a2cd4adc5591c55
Leonardo Ferreyra <104176259+LeonardoFerreyraC@users.noreply.github.com>
2023-11-20 23:44:22 -0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const auth = require('json-server-auth')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+server.use(auth)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
